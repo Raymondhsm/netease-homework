b2d60f2a06378b1bc5862745ee1acce2de3aa9a8
Raymondhsm <1260307197@qq.com>
2020-02-13 19:08:48 +0800

    修改Tick inline函数在头文件定义（修复无法解析的外部符号

--- DirectX11/Job/Apps/StepTimer.cpp
+++ DirectX11/Job/Apps/StepTimer.cpp
@@ -26,50 +26,8 @@ void StepTimer::ResetElapsedTime()
 	m_qpcSecondCounter = 0;
 }
 
-template<typename TUpdate>
-inline void StepTimer::Tick(const TUpdate & update)
-{
-	// 查询当前时间。
-	LARGE_INTEGER currentTime;
-
-	QueryPerformanceCounter(&currentTime);
-
-	__int64 timeDelta = currentTime.QuadPart - m_qpcLastTime.QuadPart;
-
-	m_qpcLastTime = currentTime;
-	m_qpcSecondCounter += timeDelta;
-
-	// 固定过大的时间增量(例如， 在调试器中暂停之后)。
-	if (timeDelta > m_qpcMaxDelta)
-	{
-		timeDelta = m_qpcMaxDelta;
-	}
-
-	// 将 QPC 单位转换为规范的计时格式。这会因为上一次固定而无法溢出。
-	timeDelta *= TicksPerSecond;
-	timeDelta /= m_qpcFrequency.QuadPart;
-
-	__int32 lastFrameCount = m_frameCount;
-
-	
-	m_elapsedTicks = timeDelta;
-	m_totalTicks += timeDelta;
-	m_leftOverTicks = 0;
-	m_frameCount++;
-
-	update();
-	
-
-	// 跟踪当前帧速率。
-	if (m_frameCount != lastFrameCount)
-	{
-		m_framesThisSecond++;
-	}
-
-	if (m_qpcSecondCounter >= static_cast<__int64>(m_qpcFrequency.QuadPart))
-	{
-		m_framesPerSecond = m_framesThisSecond;
-		m_framesThisSecond = 0;
-		m_qpcSecondCounter %= m_qpcFrequency.QuadPart;
-	}
-}
+//template<typename TUpdate>
+//inline void StepTimer::Tick(const TUpdate & update)
+//{
+//
+//}
--- DirectX11/Job/Apps/StepTimer.h
+++ DirectX11/Job/Apps/StepTimer.h
@@ -10,7 +10,52 @@ public:
 
 	// 更新计时器状态，调用适当次数的指定 Update 函数。
 	template<typename TUpdate>
-	void Tick(const TUpdate& update);
+	inline void Tick(const TUpdate& update)
+	{
+		// 查询当前时间。
+		LARGE_INTEGER currentTime;
+
+		QueryPerformanceCounter(&currentTime);
+
+		__int64 timeDelta = currentTime.QuadPart - m_qpcLastTime.QuadPart;
+
+		m_qpcLastTime = currentTime;
+		m_qpcSecondCounter += timeDelta;
+
+		// 固定过大的时间增量(例如， 在调试器中暂停之后)。
+		if (timeDelta > m_qpcMaxDelta)
+		{
+			timeDelta = m_qpcMaxDelta;
+		}
+
+		// 将 QPC 单位转换为规范的计时格式。这会因为上一次固定而无法溢出。
+		timeDelta *= TicksPerSecond;
+		timeDelta /= m_qpcFrequency.QuadPart;
+
+		__int32 lastFrameCount = m_frameCount;
+
+
+		m_elapsedTicks = timeDelta;
+		m_totalTicks += timeDelta;
+		m_leftOverTicks = 0;
+		m_frameCount++;
+
+		update();
+
+
+		// 跟踪当前帧速率。
+		if (m_frameCount != lastFrameCount)
+		{
+			m_framesThisSecond++;
+		}
+
+		if (m_qpcSecondCounter >= static_cast<__int64>(m_qpcFrequency.QuadPart))
+		{
+			m_framesPerSecond = m_framesThisSecond;
+			m_framesThisSecond = 0;
+			m_qpcSecondCounter %= m_qpcFrequency.QuadPart;
+		}
+	}
 
 	// 获取上一次 Update 调用后的运行时间。
 	__int64 GetElapsedTicks() const { return m_elapsedTicks; }
