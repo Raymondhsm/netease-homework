8ed9fda967b2e3da64841ed8bf3f6492d43bf6ee
Raymondhsm <1260307197@qq.com>
2020-02-23 02:09:39 +0800

    depthShader

--- DirectX11/Job/DepthPS.hlsl
+++ DirectX11/Job/DepthPS.hlsl
@@ -1,18 +1,13 @@
-struct GSOutput
+// 通过像素着色器传递的每个像素的颜色数据。
+struct PixelShaderInput
 {
 	float4 pos : SV_POSITION;
 };
 
-[maxvertexcount(3)]
-void main(
-	triangle float4 input[3] : SV_POSITION, 
-	inout TriangleStream< GSOutput > output
-)
+
+// (内插)颜色数据的传递函数。
+float4 main(PixelShaderInput input) : SV_TARGET
 {
-	for (uint i = 0; i < 3; i++)
-	{
-		GSOutput element;
-		element.pos = input[i];
-		output.Append(element);
-	}
+	float4 texColor = (0.f, 0.f ,0.f, 1.f);
+	return texColor; 
 }
--- DirectX11/Job/DepthVS.hlsl
+++ DirectX11/Job/DepthVS.hlsl
@@ -1,18 +1,36 @@
-struct GSOutput
+// 存储用于构成几何图形的三个基本列优先矩阵的常量缓冲区。
+cbuffer ModelViewProjectionConstantBuffer : register(b0)
 {
-	float4 pos : SV_POSITION;
+	matrix model;
+	matrix view;
+	matrix projection;
 };
 
-[maxvertexcount(3)]
-void main(
-	triangle float4 input[3] : SV_POSITION, 
-	inout TriangleStream< GSOutput > output
-)
+// 用作顶点着色器输入的每个顶点的数据。
+struct VertexShaderInput
 {
-	for (uint i = 0; i < 3; i++)
-	{
-		GSOutput element;
-		element.pos = input[i];
-		output.Append(element);
-	}
+	float3 pos :	POSITION;
+	float3 normal:	NORMAL;
+	float2 tex :	TEXCOORD;
+};
+
+// 通过像素着色器传递的每个像素的颜色数据。
+struct PixelShaderInput
+{
+	float4 pos :	SV_POSITION;
+};
+
+// 用于在 GPU 上执行顶点处理的简单着色器。
+PixelShaderInput main(VertexShaderInput input)
+{
+	PixelShaderInput output;
+	float4 pos = float4(input.pos, 1.0f);
+
+	// 将顶点位置转换为投影空间。
+	pos = mul(pos, model);
+	pos = mul(pos, view);
+	pos = mul(pos, projection);
+	output.pos = pos;
+
+	return output;
 }
